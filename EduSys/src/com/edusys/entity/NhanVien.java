911ab8da5479e6fe4a0c70e73281714e1735229c
MXThanh <thanhmxph18759@fpt.edu.vn>
2022-01-15 00:36:58 +0700

    design UI

---
+++ EduSys/src/com/edusys/entity/NhanVien.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.edusys.entity;
+
+/**
+ *
+ * @author XUÂN THÀNH
+ */
+public class NhanVien {
+    private String maNV;
+    private String matKhau;
+    private String hoTen;
+    private String sdt;
+    private boolean vaiTro;
+
+    public NhanVien() {
+    }
+
+    public NhanVien(String maNV, String matKhau, String hoTen, String sdt, boolean vaiTro) {
+        this.maNV = maNV;
+        this.matKhau = matKhau;
+        this.hoTen = hoTen;
+        this.sdt = sdt;
+        this.vaiTro = vaiTro;
+    }
+
+    public String getMaNV() {
+        return maNV;
+    }
+
+    public void setMaNV(String maNV) {
+        this.maNV = maNV;
+    }
+
+    public String getMatKhau() {
+        return matKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public boolean isVaiTro() {
+        return vaiTro;
+    }
+
+    public void setVaiTro(boolean vaiTro) {
+        this.vaiTro = vaiTro;
+    }
+    
+	
+}
